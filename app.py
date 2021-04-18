951e799b042663aae588e2eb66ac6b356b9a7b02
oliviacaitlinford <oliviacaitlinford@gmail.com>
2021-04-17 20:10:01 -0400

    model and first route added

--- app.py
+++ app.py
@@ -1,1 +1,34 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask, redirect, render_template
+from models import Cupcake, db, connect_db
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///sb_cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "aiofsf29347293sjfdv"
+
+connect_db(app)
+db.create_all()
+
+def serialize_cupcake(cupcake):
+    """Serialize cupcake SQLAlchemy object to dictionary."""
+
+    return {
+        "id": cupcake.id,
+        "flavor": cupcake.flavor,
+        "size": cupcake.size,
+        "rating": cupcake.rating,
+        "image": cupcake.image,
+    }
+
+@app.route('/api/cupcakes')
+def get_cupcakes():
+    """Fetch data about all cupcakes."""
+
+    cupcakes = Cupcake.query.all()
+    serialized = [serialize_cupcake(c) for c in cupcakes]
+
+    return jsonify(cupcakes=serialized)
--- models.py
+++ models.py
@@ -1,1 +1,20 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+class Cupcake(db.Model):
+    """Cupcake model."""
+
+    __tablename__ = 'cupcakes'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.String, nullable=False)
+    size = db.Column(db.String, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.String, nullable=False, default='https://thestayathomechef.com/wp-content/uploads/2017/12/Most-Amazing-Chocolate-Cupcakes-1-small.jpg')
+
